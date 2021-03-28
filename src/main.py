1753aa5281512a261517eb0497c5b7efc966512e
matrixgram <matrixgram@protonmail.com>
2021-03-29 02:19:45 +0430

    compelete extraxt isbn 13

--- src/main.py
+++ src/main.py
@@ -1,105 +1,21 @@
 #!/usr/bin/env python3
+import sys
+import pdfplumber
+import regex
 
 
-def is_pdf_file(path: str) -> bool:
-    """
-
-    :param path: str:
-    :returns: 4 bytes %PDF
-    references:
-        https://en.wikipedia.org/wiki/PDF
-
-        todo:
-            file exist
-            path exist
-
-    """
-    with open(path, "rb") as f:
-        return f.read(4) == b"%PDF"
+def extract_ISBN(pdf_path):
+    """Find ISBN13"""
+    pattern = "\d{3}-\d-\d{3}-\d{5}-\d" "|" "\d{3}-\d-\d{5}-\d{3}-\d"
+    pdf = pdfplumber.open(pdf_path)
+    isbn = None
+    for p in pdf.pages:
+        if p.extract_text():
+            isbn = regex.findall(pattern, p.extract_text())
+            if isbn:
+                break
+    return isbn
 
 
-def find_ISBN(path) -> str:
-    """
-
-    :param path:
-
-    """
-    from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpereter
-    from io import StringIO
-    from pdfminer.layout import LAParams
-
-    rscrmgr = PDFResourceManger()
-    retstr = StringIO()
-    codec = "utf-8"
-    laparams = LAParams()
-    device = TextConverter(rscrmgr, retstr, codec=codec, laparams=laparams)
-    fp = open(path, "rb")
-    interpreter = PDFPageInterpreter(rscrmgr, device)
-    password = ""
-    maxpages = 0
-    caching = True
-    pagenos = set()
-    for page in PDFPage.get_pages(
-        fp,
-        pagenos,
-        maxpages=maxpages,
-        password=password,
-        caching=caching,
-        check_extractable=True,
-    ):
-        text = retstr.getvalue()
-        fg.close()
-        device.close()
-        retstr.close()
-        return text
-
-
-def convert_pdf_to_txt(path):
-    """
-
-    :param path:
-
-    """
-    from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-    from pdfminer.converter import TextConverter
-    from pdfminer.layout import LAParams
-    from pdfminer.pdfpage import PDFPage
-    from io import StringIO
-
-    rsrcmgr = PDFResourceManager()
-    retstr = StringIO()
-    codec = "utf-8"
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-    fp = open(path, "rb")
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    password = ""
-    maxpages = 0
-    caching = True
-    pagenos = set()
-
-    for page in PDFPage.get_pages(
-        fp,
-        pagenos,
-        maxpages=maxpages,
-        password=password,
-        caching=caching,
-        check_extractable=True,
-    ):
-        interpreter.process_page(page)
-
-        text = retstr.getvalue()
-
-        fp.close()
-        device.close()
-        retstr.close()
-        return text
-
-
-if "__main__" == __name__:
-    import sys
-
-    # print(is_pdf_file(sys.argv[1]))
-    import pdfplumber
-
-    pdf = pdfplumber.open("a/home/matrixgram/Documents/ISBN/9780262033848.pdf")
+if __name__ == "__main__":
+    print(extract_ISBN(sys.argv[1]))
